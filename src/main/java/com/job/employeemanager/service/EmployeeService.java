0156233af36cd057ff48fefd08c659595cb4fc13
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-06-06 01:41:51 +0300

    employee repository

---
+++ src/main/java/com/job/employeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.job.employeemanager.service;
+
+import com.job.employeemanager.model.Employee;
+import com.job.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+}
